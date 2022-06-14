17b189ccd00a6df3f8761cc91028871d67b32c45
Asus ROG <terry4715@gmail.com>
2022-06-14 09:30:46 +0100

    Relative reference removed

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from .database import Database, CursorFromPool
+from database import Database, CursorFromPool
 from flask import Flask, render_template, request
 import json
 import datetime
